54cae282ca23e97288e445c9e001baf92ec2c2cc
Oussama Thr <110485131+Oussama-Thr@users.noreply.github.com>
2024-12-24 23:39:08 +0000

    Update CodeGenerator.jsx

--- frontend1/src/components/CodeGenerator.jsx
+++ frontend1/src/components/CodeGenerator.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import "../style/DiagramEditor.css";
 
 const CodeGenerator = ({ graph }) => {
   const [generatedCode, setGeneratedCode] = useState("");
@@ -29,10 +30,13 @@ const CodeGenerator = ({ graph }) => {
   const generateCode = async () => {
     const diagramData = extractDiagramData();
     try {
-      const response = await axios.post("http://localhost:8000/api/generate-code", {
-        diagramData,
-        language,
-      });
+      const response = await axios.post(
+        "http://localhost:8000/api/generate-code",
+        {
+          diagramData,
+          language,
+        }
+      );
       setGeneratedCode(response.data.code);
     } catch (error) {
       console.error("Error generating code:", error);
@@ -41,19 +45,21 @@ const CodeGenerator = ({ graph }) => {
 
   return (
     <div>
-      <button onClick={generateCode}>Generate Code</button>
-      <select value={language} onChange={(e) => setLanguage(e.target.value)}>
-        <option value="Java">Java</option>
-        <option value="PHP">PHP</option>
-        <option value="Python">Python</option>
-      </select>
+      <div className="select-button-container" style={{ marginTop : 10}}>
+        <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+          <option value="Java">Java</option>
+          <option value="PHP">PHP</option>
+          <option value="Python">Python</option>
+        </select>
+        <button className="button" onClick={generateCode}>Generate Code</button>
+      </div>
       {generatedCode && (
         <textarea
           value={generatedCode}
           readOnly
           rows={10}
           cols={50}
-          style={{ marginTop: "10px", width: "100%" }}
+          style={{ marginTop: "10px", width: "100%" , height : "100%"}}
         />
       )}
     </div>
