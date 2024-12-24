aa4c23de10259a9e778f5c4adbf163fd70f9a5d7
Oussama Thr <110485131+Oussama-Thr@users.noreply.github.com>
2024-12-24 23:38:35 +0000

    Update App.js

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -3,11 +3,11 @@ import DiagramEditor from "./components/DiagramEditor";
 import CodeGenerator from "./components/CodeGenerator";
 
 const App = () => {
-  const [graph, setGraph] = useState(null); // State to manage the graph instance
+  const [graph, setGraph] = useState(null);
 
   return (
     <div>
-      <h1>UML Diagram Editor</h1>
+      <h1 style={{ textAlign: 'center' }}>UML Diagram Editor</h1>
       <DiagramEditor setGraph={setGraph} />
       {graph && <CodeGenerator graph={graph} />}
     </div>
